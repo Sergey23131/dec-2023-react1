5c11eab7d0b4ef510cf50c6331a66c46b21c746a
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2023-08-22 22:13:47 -0500

    ud

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 
-import App from './App';
+
+
 import {setupStore} from './redux';
+import {App} from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
